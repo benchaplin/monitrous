f3484cbb45f4edab3decbe075ceae2e34f1a317b
Ben Chaplin <benchaplin@protonmail.ch>
2022-12-11 16:57:10 -0500

    format

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use std::path::{PathBuf, Path};
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
 use clap::{Parser, Subcommand};
 use dssim::{Dssim, Val};
-use headless_chrome::{Browser, Tab};
 use headless_chrome::protocol::cdp::Page::CaptureScreenshotFormatOption;
 use headless_chrome::types::Bounds;
+use headless_chrome::{Browser, Tab};
 use image::ImageFormat;
 use indicatif::ProgressBar;
 
-/// Monitor a list of URLs by capturing screenshots and comparing them 
+/// Monitor a list of URLs by capturing screenshots and comparing them
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 #[clap(override_usage = "
@@ -17,7 +17,7 @@ use indicatif::ProgressBar;
   monitrous compare <NEW_DIR> <OLD_DIR>")]
 struct Cli {
     #[command(subcommand)]
-    action: Action
+    action: Action,
 }
 
 #[derive(Subcommand, Debug)]
@@ -38,10 +38,9 @@ enum Action {
         new_dir: PathBuf,
         /// Old directory for comparison
         #[clap(value_parser)]
-        old_dir: PathBuf
-    }
+        old_dir: PathBuf,
+    },
 }
-
 
 fn read_file(filename: &Path) -> Vec<String> {
     let contents = std::fs::read_to_string(filename).unwrap();
@@ -49,17 +48,20 @@ fn read_file(filename: &Path) -> Vec<String> {
 }
 
 fn set_dimensions(tab: &Arc<Tab>, width: Option<f64>, height: Option<f64>) {
-    tab.set_bounds(Bounds::Normal{ 
-        left: Some(0), 
-        top: Some(0), 
+    tab.set_bounds(Bounds::Normal {
+        left: Some(0),
+        top: Some(0),
         width,
-        height
-    }).ok(); 
+        height,
+    })
+    .ok();
 }
 
 fn get_doc_height(tab: &Arc<Tab>) -> f64 {
     let doc = tab.wait_for_element("html").unwrap();
-    let remote_height = doc.call_js_fn(r#"
+    let remote_height = doc
+        .call_js_fn(
+            r#"
         function getPageHeight() {
             const body = document.body;
             const html = document.documentElement;
@@ -71,7 +73,11 @@ fn get_doc_height(tab: &Arc<Tab>) -> f64 {
                 html.offsetHeight 
             );
         }
-    "#, Vec::new(), false).unwrap();
+    "#,
+            Vec::new(),
+            false,
+        )
+        .unwrap();
 
     remote_height.value.unwrap().as_f64().unwrap()
 }
@@ -79,18 +85,17 @@ fn get_doc_height(tab: &Arc<Tab>) -> f64 {
 fn take_screenshot(tab: &Arc<Tab>, url: &str) -> Vec<u8> {
     set_dimensions(&tab, Some(1200.0), None);
 
-    tab.navigate_to(url).unwrap()
-        .wait_until_navigated().unwrap();
-   
+    tab.navigate_to(url)
+        .unwrap()
+        .wait_until_navigated()
+        .unwrap();
+
     let height = get_doc_height(&tab);
     set_dimensions(&tab, Some(1200.0), Some(height));
 
-    let png_data = tab.capture_screenshot(
-        CaptureScreenshotFormatOption::Png,
-        None,
-        None,
-        true 
-    ).unwrap();
+    let png_data = tab
+        .capture_screenshot(CaptureScreenshotFormatOption::Png, None, None, true)
+        .unwrap();
 
     png_data
 }
@@ -99,9 +104,10 @@ fn export_png(png_bytes: Vec<u8>, output_dir: &Path, filename: String) {
     image::load_from_memory_with_format(&png_bytes, ImageFormat::PNG).unwrap();
     std::fs::create_dir_all(output_dir).ok();
     std::fs::write(
-        format!("{}/{}", output_dir.to_str().unwrap(), filename), 
-        png_bytes
-    ).unwrap();
+        format!("{}/{}", output_dir.to_str().unwrap(), filename),
+        png_bytes,
+    )
+    .unwrap();
 }
 
 fn take_screenshots(urls: Vec<String>, output_dir: &Path) {
@@ -112,7 +118,7 @@ fn take_screenshots(urls: Vec<String>, output_dir: &Path) {
         let tab = browser.new_tab().unwrap();
         let png_bytes = take_screenshot(&tab, &url);
         let filename = format!(
-            "{}.png", 
+            "{}.png",
             url.replace(":", "_").replace("/", "_").replace(".", "_")
         );
         export_png(png_bytes, output_dir, filename);
@@ -131,7 +137,7 @@ fn compare_imgs(img1: &Path, img2: &Path) -> Val {
     let attr = Dssim::new();
     let dssim_img1 = dssim::load_image(&attr, img1).unwrap();
     let dssim_img2 = dssim::load_image(&attr, img2).unwrap();
-    let (diff, _) = attr.compare(&dssim_img1, &dssim_img2); 
+    let (diff, _) = attr.compare(&dssim_img1, &dssim_img2);
 
     diff
 }
@@ -156,7 +162,7 @@ fn compare_screenshots(old_dir: &Path, new_dir: &Path) {
         .filter_map(|e| e.ok())
         .map(|e| e.path().file_name().unwrap().to_str().unwrap().to_string())
         .collect::<Vec<_>>();
-    
+
     let pb = ProgressBar::new(new_paths.len() as u64);
     for path in new_paths {
         if old_paths.contains(&path) {
@@ -166,7 +172,10 @@ fn compare_screenshots(old_dir: &Path, new_dir: &Path) {
             pb.println(format!("comparing {}", &path));
             let diff = compare_imgs(old_path, new_path);
             if diff > 0.0 {
-                pb.println(format!("diff: {}, generating diff image for {}", diff, &path));
+                pb.println(format!(
+                    "diff: {}, generating diff image for {}",
+                    diff, &path
+                ));
                 save_diff_img(old_path, new_path, Path::new(&path));
             }
             pb.inc(1);
@@ -178,7 +187,10 @@ fn main() {
     let args = Cli::parse();
 
     match &args.action {
-        Action::Capture { input_file, output_dir } => {
+        Action::Capture {
+            input_file,
+            output_dir,
+        } => {
             let urls = read_file(&input_file.as_path());
             take_screenshots(urls, &output_dir.as_path());
         }
